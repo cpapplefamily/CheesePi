e6665f0d72f3bff3bb93ddd873cb62b99150f1b2
cpapplefamily <cpapplefamily@gmail.com>
2021-07-10 22:31:23 -0500

    init

---
+++ cheesePi.py
@@ -0,0 +1,281 @@
+import os
+import requests
+import time
+import _thread as thread
+import websocket
+import RPi.GPIO as GPIO
+import json
+import argparse
+import cheesePowerPortRGB as RGB
+from rpi_ws281x import Color
+
+last_matchstate = 0
+total_cells = 0
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+FMS_IP = "10.0.100.05"
+FMS_PORT = "8080"
+FMS_SERVER = FMS_IP + ":" + FMS_PORT
+ALLIANCE_COLOR = 'red' # Change accordingly
+#ALLIANCE_COLOR = 'blue' # Change accordingly
+USERNAME = 'root'
+PASSWORD = 'root'
+
+goal_char_msg_map = {
+    "I": '{ "type": "CI" }',
+    "O": '{ "type": "CO" }',
+    "L": '{ "type": "CL" }'
+}
+
+#Global Counters
+innerCount = 0
+outerCount = 0
+lowerCount = 0
+
+#opto Issolator Channle to GPIO_Pin
+optoIssolator_1 = 7
+optoIssolator_2 = 11
+optoIssolator_3 = 13
+optoIssolator_4 = 15
+
+#GPIO Assignments
+innerCount_Pin = optoIssolator_1
+outerCount1_Pin = optoIssolator_2
+outerCount2_Pin = optoIssolator_3
+lowerCount_Pin = optoIssolator_4
+
+LED_Pin = 32
+
+GPIO.setup(LED_Pin,GPIO.OUT)
+GPIO.output(LED_Pin,0)
+
+
+###############################
+## Inerner Count Input setup ##
+###############################
+GPIO.setup(innerCount_Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+def inner_callback(channel):
+    global innerCount
+    innerCount += 1
+    
+GPIO.add_event_detect(innerCount_Pin,GPIO.RISING,callback=inner_callback, bouncetime=200)
+
+##############################
+## Outer Count1 Input setup ##
+##############################
+GPIO.setup(outerCount1_Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+def outer1_callback(channel):
+    global outerCount
+    outerCount += 1
+    
+GPIO.add_event_detect(outerCount1_Pin,GPIO.RISING,callback=outer1_callback, bouncetime=200)
+
+##############################
+## Outer Count2 Input setup ##
+##############################
+GPIO.setup(outerCount2_Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+def outer2_callback(channel):
+    global outerCount
+    outerCount += 1
+    
+GPIO.add_event_detect(outerCount2_Pin,GPIO.RISING,callback=outer2_callback, bouncetime=200)
+
+#############################
+## Lower Count Input setup ##
+#############################
+GPIO.setup(lowerCount_Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+def lower_callback(channel):
+    global lowerCount
+    lowerCount += 1
+    
+GPIO.add_event_detect(lowerCount_Pin,GPIO.RISING,callback=lower_callback, bouncetime=200)
+
+#Function to wait for a Power Cell to be scored
+def get_power_cell_to_count():
+    global innerCount
+    global outerCount
+    global lowerCount
+    while(outerCount == 0 and innerCount == 0 and lowerCount == 0):
+        one = 1
+    return True
+
+#Retrieve the keyStroke char for the goal to be tallied
+def get_msg_from_goal_char(goal_char):
+    return goal_char_msg_map[goal_char]
+
+def blink_led():
+    if GPIO.input(LED_Pin):
+        #myfill(Color(255, 0, 0))
+        RGB.strip_set_LED(0,RGB.LED_SOL,Color(63,63,63))
+        GPIO.output(LED_Pin,0)
+    else:
+        #myfill(Color(0, 0, 255))
+        RGB.strip_set_LED(0,RGB.LED_SOL,Color(255,255,255))
+        GPIO.output(LED_Pin,1)
+        
+
+
+def ws_msg(self, msg):
+    global last_matchstate
+    global total_cells
+    blink_led()
+    j = json.loads(msg)
+    print(f"MSG Type {j['type']}")
+    if j['type'] == "matchTime":
+        #print("Is MatchTime") 
+        if "data" in j:
+            #print("data")
+            d = j['data']
+            #print(d)
+            if "MatchState" in d:
+                m_state = d['MatchState']
+                if m_state == 0 and m_state != last_matchstate:
+                    last_matchstate = m_state
+                elif m_state == 1 and m_state != last_matchstate:                    
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                elif m_state == 2 and m_state != last_matchstate:                    
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                elif m_state == 3 and m_state != last_matchstate:                    
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                elif m_state == 4 and m_state != last_matchstate:                    
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                elif m_state == 5 and m_state != last_matchstate:
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                elif m_state == 6 and m_state != last_matchstate:                    
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)                    
+                else:
+                    last_matchstate = m_state
+                    #strip_control(last_matchstate, total_cells)
+                    
+    else:
+        if j['type'] == "realtimeScore":
+            #print("Is RealtimeScore") 
+            if "data" in j:
+                #print("data")
+                d = j['data']
+                if "ScoreSummary" in d["Red"]:
+                    x = d["Red"]
+                    s = x['ScoreSummary']
+                    if "TotalCells" in s:
+                        total_cells = s["TotalCells"]
+                        print(f"Total Power Cells:           {c}")
+                        
+    RGB.strip_control(ALLIANCE_COLOR, last_matchstate, total_cells) 
+                        #strip_control(last_matchstate, total_cells)
+                        #useable = strip.numPixels() - LED_SOL
+                        #strip_set_LED(LED_SOL,strip.numPixels(),Color(0, 0, 0))
+                        #strip_set_LED(LED_SOL,int(c/30*useable),get_aliance_color_RGB(ALLIANCE_COLOR))
+                    
+
+    
+        
+
+
+    
+def get_on_ws_open_callback():
+    def on_ws_open(ws):
+        print("Connected to FMS")
+
+        def run(*args):
+            while(True):
+                global innerCount
+                global outerCount
+                global lowerCount
+                #Check for any counters that need to be tallied
+                sendData = get_power_cell_to_count()
+                #What Counter?
+                if (outerCount > 0):
+                    goal_char = "O"
+                    outerCount -= 1
+                elif (innerCount > 0):
+                    goal_char = "I"
+                    innerCount -= 1
+                elif (lowerCount > 0):
+                    goal_char = "L"
+                    lowerCount -= 1
+                else:
+                    print("nothing to Score")
+                    goal_char = ""
+                    
+                    
+                print(f'Info: received "{goal_char}"')
+                
+                if (goal_char in goal_char_msg_map):
+                    print(f'Info: sent {get_msg_from_goal_char(goal_char)}')
+                    try:
+                        ws.send(get_msg_from_goal_char(goal_char))
+                    except Exception:
+                        open_websocket()
+                else:
+                    print('Error: unknown char received')
+
+        thread.start_new_thread(run, ())
+    
+    return on_ws_open
+    
+def open_websocket():
+    def reopen_websocket():
+        print("attempt to ReOpen Websocket")
+        open_websocket()
+    
+    print("request.post")
+    res = requests.post(f'http://{FMS_SERVER}/login'
+        , data={'username': USERNAME, 'password': PASSWORD}
+        , allow_redirects=False
+    )
+        
+    print("Create ws")
+    ws = websocket.WebSocketApp(f'ws://{FMS_SERVER}/panels/scoring/{ALLIANCE_COLOR}/websocket'
+        , on_message = ws_msg
+        , on_open=get_on_ws_open_callback()
+        , on_close=reopen_websocket
+        , cookie="; ".join(["%s=%s" %(i, j) for i, j in res.cookies.get_dict().items()])
+    )
+
+    #websocket.enableTrace(True)
+    
+    print("Run Forever")
+    ws.run_forever()
+    
+
+def main():
+    """Loops until a connection is made to the FMS.
+    When connected opens a websocket 
+    """
+    #led_count = 100
+    #led_pin = 18
+    #led_sol = 1
+    #led_max_score = 30
+    #RGB.create_strip(100, 18, 1, 30,RGB.get_aliance_color_RGB(ALLIANCE_COLOR))
+    RGB.create_strip(RGB.get_aliance_color_RGB(ALLIANCE_COLOR))
+    
+    #Wait for Network connection to FMS
+    while(True):
+        print(f'Check Network Connection {FMS_IP}')
+        response = os.system("ping -c 1 " + FMS_IP)
+        if response == 0:
+            print(f'{FMS_IP} Found')
+        else:
+            print("Network Error")
+        if(response == 0): break
+        time.sleep(2)
+    time.sleep(1)
+    print("Open Web Socket")
+    open_websocket()
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ cheesePowerPortRGB.py
@@ -0,0 +1,137 @@
+from rpi_ws281x import *
+
+# LED strip configuration:
+strip = None
+LED_COUNT      	= 300      # Number of LED pixels.
+LED_PIN        	= 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    	= 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        	= 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS 	= 10     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     	= False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    	= 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_SOL		= 5		#Number of Sign Of Life LED's 
+LED_MAX_SCORE  	= 30		# Power Cell Max Score
+
+
+matchStatus_map = {
+	0: "Feild Safe",
+	1: "pre match",
+	2: "pre match",
+	3: "in Auto",
+	4: "Pause After Auto",
+	5: "in match",
+	6: "Match Complete"
+}
+
+
+field_color_map = {
+    "green": Color(0,255,0),
+    "violet": Color(150,0,150),
+    "yellow": Color(255,255,0)
+}
+
+aliance_colors_map = {
+    "red": Color(255, 0, 0),
+    "green": Color(0, 255, 0),
+    "blue": Color(0, 0, 255)
+}
+
+def configStrip(led_count, led_pin, led_sol, max_score):
+    LED_COUNT      = 300      # Number of LED pixels.
+    LED_PIN
+    LED_SOL       = 5
+    LED_MAX_SCORE  = 30
+    
+
+def create_strip(color):
+    global strip
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    for i in range(0,strip.numPixels(),4):
+        strip.setPixelColor(i, color)
+        strip.setPixelColor(i+1, Color(0,0,0))
+        strip.setPixelColor(i+2, Color(0,0,0))
+        strip.setPixelColor(i+3, Color(0,0,0))
+    strip.show()
+    
+def get_matchStatus_txt(status):
+	return matchStatus_map[status]
+	
+def get_field_color_RGB(status_color):
+    return field_color_map[status_color]
+    
+def get_aliance_color_RGB(alliance_color):
+    return aliance_colors_map[alliance_color]
+	
+def strip_set_LED(startLED, lastLED, color):
+	if lastLED > strip.numPixels():
+		lastLED = strip.numPixels()
+	for i in range(startLED,lastLED):
+		strip.setPixelColor(i, color)
+	strip.show()
+    
+def myfill(color):
+    print("myFill enter")
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.show()
+
+def strip_control(alliance_color, matchstate, total_cells):
+	#printFieldState(matchstate)
+	if matchstate == 0:
+		strip_set_LED(LED_SOL,strip.numPixels(),get_field_color_RGB("green"))
+	elif matchstate == 1:
+		strip_set_LED(LED_SOL,strip.numPixels(),Color(0, 0, 0))
+	elif matchstate == 2:
+		strip_set_LED(LED_SOL,strip.numPixels(),Color(0, 0, 0))
+	elif matchstate == 3:
+		strip_set_LED(LED_SOL,strip.numPixels(),get_field_color_RGB("yellow"))
+	elif matchstate == 4:
+		pass
+		#strip_set_LED(LED_SOL,strip.numPixels(),Color(0, 0, 0))
+	elif matchstate == 5:
+		if total_cells > LED_MAX_SCORE:
+			theaterChase(alliance_color)
+		else:
+			LED_Score(alliance_color, total_cells)
+	elif matchstate == 6:
+		strip_set_LED(LED_SOL,strip.numPixels(),get_field_color_RGB("violet"))
+	else:
+		pass
+	
+def LED_Score(alliance_color, total_cells):
+	useable = strip.numPixels() - LED_SOL
+	""" Fill strip with Percentage relitive to max"""
+	for i in range(LED_SOL,int(total_cells/LED_MAX_SCORE*useable)):
+		strip.setPixelColor(i, get_aliance_color_RGB(alliance_color))
+	""" Fill remaining strip off"""
+	for i in range(int(total_cells/LED_MAX_SCORE*useable),LED_COUNT):
+		strip.setPixelColor(i, Color(0,0,0))
+	strip.show()
+	
+def theaterChase(alliance_color):
+    """Movie theater light style chaser animation."""
+    for i in range(LED_SOL, strip.numPixels(), 2):
+        strip.setPixelColor(i, get_aliance_color_RGB(alliance_color))
+    for i in range(LED_SOL, strip.numPixels(), 2):
+        strip.setPixelColor(i+1, Color(100,100,100))
+    
+    strip.show()
+
+def xtheaterChase(alliance_color):
+    """Movie theater light style chaser animation."""
+    for q in range(3):
+        for i in range(LED_SOL, strip.numPixels(), 3):
+            strip.setPixelColor(i+q, get_aliance_color_RGB(alliance_color))
+        strip.show()
+        for i in range(LED_SOL, strip.numPixels(), 3):
+            strip.setPixelColor(i+q, 0)
+            
+            
+def printFieldState(matchstate):
+	print('************************')
+	print(f'MatchStatus: {get_matchStatus_txt(matchstate)}')
+	print('************************')
