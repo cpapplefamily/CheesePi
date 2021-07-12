fa0e1cf228145b59bb279015dfb77b03585d7924
cpapplefamily <cpapplefamily@gmail.com>
2021-07-11 21:28:07 -0500

    Added small delay when LED initilize to see color

--- cheesePowerPortRGB.py
+++ cheesePowerPortRGB.py
@@ -1,4 +1,5 @@
 from rpi_ws281x import *
+import time
 
 # LED strip configuration:
 strip = None
@@ -117,6 +118,7 @@ def create_default_strip(color):
         strip.setPixelColor(i+2, Color(0,0,0))
         strip.setPixelColor(i+3, Color(0,0,0))
     strip.show()
+    time.sleep(2)
    
 """
 Helpers to get variables
